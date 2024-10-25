2cf1ab79c4c5e61c4850a2ffa10c78f214c05d0c
Dauud10 <k00263519@student.tus.ie>
2024-10-25 14:36:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('fetch-button').addEventListener('click', fetchData);
+
+function fetchData() {
+    const dataContainer = document.getElementById('dataContainer');
+
+    
+    const dataBox = document.createElement('div');
+    dataBox.className = 'data-box';
+
+    const img = document.createElement('img');
+    img.src = (https://picsum.photos/400/300?random)
+}
